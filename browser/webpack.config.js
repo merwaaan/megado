4750269ad15675f51f50159459841673c7736ed4
Merwan Achibet <achibet.merwan@gmail.com>
2016-12-14 12:07:45 +0100

    Make is part of the webpack bundling

--- browser/webpack.config.js
+++ browser/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const shellPlugin = require('webpack-shell-plugin');
 
 const PATHS = {
     build: path.resolve(__dirname, 'build'),
     components: path.resolve(__dirname, 'components')
 };
+
+// TODO add some linter?
 
 module.exports = {
     entry: PATHS.components + '/App.jsx',
@@ -17,6 +20,16 @@ module.exports = {
         root: path.resolve(__dirname),
         extensions: ['', '.js', '.jsx', '.scss']
     },
+
+    plugins: [
+        new shellPlugin({
+            // Compile to JS before bundling
+            // TODO would be nice to pack the output in the bundle
+            onBuildStart: ['make'],
+            // Required to re-run the script in watch mode
+            dev: false
+        })
+    ],
 
     module: {
         loaders: [
