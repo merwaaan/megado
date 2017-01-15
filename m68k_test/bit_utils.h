9302f7ee3154e3601c8904a0a9afd7f92d16e74d
Merwan Achibet <achibet.merwan@gmail.com>
2017-01-15 16:08:04 +0100

    Clean up bit utils tests

--- m68k_test/bit_utils.h
+++ m68k_test/bit_utils.h
@@ -7,122 +7,122 @@
 
 MU_TEST(test_bit)
 {
-    mu_assert_int_eq_bin(0, BIT(0, 0));
-    mu_assert_int_eq_bin(0, BIT(0, 5));
+    ASSERT_BIN(0, BIT(0, 0));
+    ASSERT_BIN(0, BIT(0, 5));
 
-    mu_assert_int_eq_bin(1, BIT(1, 0));
-    mu_assert_int_eq_bin(0, BIT(1, 1));
+    ASSERT_BIN(1, BIT(1, 0));
+    ASSERT_BIN(0, BIT(1, 1));
 
-    mu_assert_int_eq_bin(1, BIT(0x80, 7));
-    mu_assert_int_eq_bin(0, BIT(0x80, 6));
-    mu_assert_int_eq_bin(0, BIT(0x80, 8));
+    ASSERT_BIN(1, BIT(0x80, 7));
+    ASSERT_BIN(0, BIT(0x80, 6));
+    ASSERT_BIN(0, BIT(0x80, 8));
 }
 
 MU_TEST(test_bit_set)
 {
-    mu_assert_int_eq_bin(1, BIT_SET(0, 0));
-    mu_assert_int_eq_bin(1, BIT_SET(1, 0));
+    ASSERT_BIN(1, BIT_SET(0, 0));
+    ASSERT_BIN(1, BIT_SET(1, 0));
 
-    mu_assert_int_eq_bin(8, BIT_SET(0, 3));
-    mu_assert_int_eq_bin(9, BIT_SET(9, 3));
+    ASSERT_BIN(8, BIT_SET(0, 3));
+    ASSERT_BIN(9, BIT_SET(9, 3));
 }
 
 MU_TEST(test_bit_clr)
 {
-    mu_assert_int_eq_bin(0, BIT_CLR(0, 0));
-    mu_assert_int_eq_bin(0, BIT_CLR(1, 0));
+    ASSERT_BIN(0, BIT_CLR(0, 0));
+    ASSERT_BIN(0, BIT_CLR(1, 0));
 
-    mu_assert_int_eq_bin(0, BIT_CLR(0, 3));
-    mu_assert_int_eq_bin(1, BIT_CLR(9, 3));
+    ASSERT_BIN(0, BIT_CLR(0, 3));
+    ASSERT_BIN(1, BIT_CLR(9, 3));
 
-    mu_assert_int_eq_bin(0xDF, BIT_CLR(0xFF, 5));
+    ASSERT_BIN(0xDF, BIT_CLR(0xFF, 5));
 }
 
 MU_TEST(test_bit_chg)
 {
-    mu_assert_int_eq_bin(0, BIT_CHG(0, 0, 0));
-    mu_assert_int_eq_bin(1, BIT_CHG(0, 0, 1));
+    ASSERT_BIN(0, BIT_CHG(0, 0, 0));
+    ASSERT_BIN(1, BIT_CHG(0, 0, 1));
 
-    mu_assert_int_eq_bin(0, BIT_CHG(0, 3, 0));
-    mu_assert_int_eq_bin(8, BIT_CHG(0, 3, 1));
+    ASSERT_BIN(0, BIT_CHG(0, 3, 0));
+    ASSERT_BIN(8, BIT_CHG(0, 3, 1));
 
-    mu_assert_int_eq_bin(0xFE, BIT_CHG(0xFF, 0, 0));
-    mu_assert_int_eq_bin(0xFF, BIT_CHG(0xFF, 0, 1));
-    mu_assert_int_eq_bin(0x7F, BIT_CHG(0xFF, 7, 0));
+    ASSERT_BIN(0xFE, BIT_CHG(0xFF, 0, 0));
+    ASSERT_BIN(0xFF, BIT_CHG(0xFF, 0, 1));
+    ASSERT_BIN(0x7F, BIT_CHG(0xFF, 7, 0));
 }
 
 MU_TEST(test_fragment)
 {
-    mu_assert_int_eq_bin(0, FRAGMENT(0, 0, 0));
-    mu_assert_int_eq_bin(0, FRAGMENT(0, 7, 0));
+    ASSERT_BIN(0, FRAGMENT(0, 0, 0));
+    ASSERT_BIN(0, FRAGMENT(0, 7, 0));
 
-    mu_assert_int_eq_bin(0, FRAGMENT(2, 0, 0));
-    mu_assert_int_eq_bin(2, FRAGMENT(2, 1, 0));
-    mu_assert_int_eq_bin(1, FRAGMENT(2, 1, 1));
+    ASSERT_BIN(0, FRAGMENT(2, 0, 0));
+    ASSERT_BIN(2, FRAGMENT(2, 1, 0));
+    ASSERT_BIN(1, FRAGMENT(2, 1, 1));
 
-    mu_assert_int_eq_bin(0xFF, FRAGMENT(0xFF, 7, 0));
-    mu_assert_int_eq_bin(0xFF, FRAGMENT(0xFF, 20, 0));
-    mu_assert_int_eq_bin(0x0F, FRAGMENT(0xFF, 3, 0));
-    mu_assert_int_eq_bin(0x0F, FRAGMENT(0xFF, 7, 4));
+    ASSERT_BIN(0xFF, FRAGMENT(0xFF, 7, 0));
+    ASSERT_BIN(0xFF, FRAGMENT(0xFF, 20, 0));
+    ASSERT_BIN(0x0F, FRAGMENT(0xFF, 3, 0));
+    ASSERT_BIN(0x0F, FRAGMENT(0xFF, 7, 4));
 }
 
 MU_TEST(test_mask_below)
 {
-    mu_assert_int_eq_bin(0, MASK_BELOW(0, 0));
-    mu_assert_int_eq_bin(1, MASK_BELOW(1, 0));
-    mu_assert_int_eq_bin(3, MASK_BELOW(3, 0));
-    mu_assert_int_eq_bin(2, MASK_BELOW(3, 1));
-    mu_assert_int_eq_bin(0xFF, MASK_BELOW(0xFF, 0));
-    mu_assert_int_eq_bin(0xF8, MASK_BELOW(0xFF, 3));
+    ASSERT_BIN(0, MASK_BELOW(0, 0));
+    ASSERT_BIN(1, MASK_BELOW(1, 0));
+    ASSERT_BIN(3, MASK_BELOW(3, 0));
+    ASSERT_BIN(2, MASK_BELOW(3, 1));
+    ASSERT_BIN(0xFF, MASK_BELOW(0xFF, 0));
+    ASSERT_BIN(0xF8, MASK_BELOW(0xFF, 3));
 }
 
 MU_TEST(test_mask_below_inc)
 {
-    mu_assert_int_eq_bin(0, MASK_BELOW_INC(0, 0));
-    mu_assert_int_eq_bin(0, MASK_BELOW_INC(1, 0));
-    mu_assert_int_eq_bin(2, MASK_BELOW_INC(3, 0));
-    mu_assert_int_eq_bin(0, MASK_BELOW_INC(3, 1));
-    mu_assert_int_eq_bin(0xFE, MASK_BELOW_INC(0xFF, 0));
-    mu_assert_int_eq_bin(0xF0, MASK_BELOW_INC(0xFF, 3));
+    ASSERT_BIN(0, MASK_BELOW_INC(0, 0));
+    ASSERT_BIN(0, MASK_BELOW_INC(1, 0));
+    ASSERT_BIN(2, MASK_BELOW_INC(3, 0));
+    ASSERT_BIN(0, MASK_BELOW_INC(3, 1));
+    ASSERT_BIN(0xFE, MASK_BELOW_INC(0xFF, 0));
+    ASSERT_BIN(0xF0, MASK_BELOW_INC(0xFF, 3));
 }
 
 MU_TEST(test_mask_above)
 {
-    mu_assert_int_eq_bin(0, MASK_ABOVE(0, 0));
-    mu_assert_int_eq_bin(1, MASK_ABOVE(1, 0));
-    mu_assert_int_eq_bin(1, MASK_ABOVE(3, 0));
-    mu_assert_int_eq_bin(3, MASK_ABOVE(3, 1));
-    mu_assert_int_eq_bin(1, MASK_ABOVE(0xFF, 0));
-    mu_assert_int_eq_bin(0x0F, MASK_ABOVE(0xFF, 3));
+    ASSERT_BIN(0, MASK_ABOVE(0, 0));
+    ASSERT_BIN(1, MASK_ABOVE(1, 0));
+    ASSERT_BIN(1, MASK_ABOVE(3, 0));
+    ASSERT_BIN(3, MASK_ABOVE(3, 1));
+    ASSERT_BIN(1, MASK_ABOVE(0xFF, 0));
+    ASSERT_BIN(0x0F, MASK_ABOVE(0xFF, 3));
 }
 
 MU_TEST(test_mask_above_inc)
 {
-    mu_assert_int_eq_bin(0, MASK_ABOVE_INC(0, 0));
-    mu_assert_int_eq_bin(0, MASK_ABOVE_INC(1, 0));
-    mu_assert_int_eq_bin(0, MASK_ABOVE_INC(3, 0));
-    mu_assert_int_eq_bin(1, MASK_ABOVE_INC(3, 1));
-    mu_assert_int_eq_bin(1, MASK_ABOVE_INC(0xFF, 1));
-    mu_assert_int_eq_bin(7, MASK_ABOVE_INC(0xFF, 3));
+    ASSERT_BIN(0, MASK_ABOVE_INC(0, 0));
+    ASSERT_BIN(0, MASK_ABOVE_INC(1, 0));
+    ASSERT_BIN(0, MASK_ABOVE_INC(3, 0));
+    ASSERT_BIN(1, MASK_ABOVE_INC(3, 1));
+    ASSERT_BIN(1, MASK_ABOVE_INC(0xFF, 1));
+    ASSERT_BIN(7, MASK_ABOVE_INC(0xFF, 3));
 }
 
 MU_TEST(test_bin_parse)
 {
-    mu_assert_int_eq_bin(0, bin_parse("0"));
-    mu_assert_int_eq_bin(0, bin_parse("000"));
-    mu_assert_int_eq_bin(0, bin_parse("0 0    0"));
-    mu_assert_int_eq_bin(0, bin_parse(""));
-    mu_assert_int_eq_bin(0, bin_parse("   "));
+    ASSERT_BIN(0, bin_parse("0"));
+    ASSERT_BIN(0, bin_parse("000"));
+    ASSERT_BIN(0, bin_parse("0 0    0"));
+    ASSERT_BIN(0, bin_parse(""));
+    ASSERT_BIN(0, bin_parse("   "));
 
-    mu_assert_int_eq_bin(1, bin_parse("1"));
-    mu_assert_int_eq_bin(1, bin_parse("00 01"));
+    ASSERT_BIN(1, bin_parse("1"));
+    ASSERT_BIN(1, bin_parse("00 01"));
 
-    mu_assert_int_eq_bin(3, bin_parse("11"));
-    mu_assert_int_eq_bin(3, bin_parse("     1   1     "));
+    ASSERT_BIN(3, bin_parse("11"));
+    ASSERT_BIN(3, bin_parse("     1   1     "));
 
-    mu_assert_int_eq_bin(0xCC, bin_parse("11001100"));
-    mu_assert_int_eq_bin(0xE3, bin_parse("1110 0011"));
-    mu_assert_int_eq_bin(0xFF, bin_parse("1111 1111"));
+    ASSERT_BIN(0xCC, bin_parse("11001100"));
+    ASSERT_BIN(0xE3, bin_parse("1110 0011"));
+    ASSERT_BIN(0xFF, bin_parse("1111 1111"));
 }
 
 MU_TEST(test_bin_tostring)
@@ -140,11 +140,14 @@ MU_TEST_SUITE(test_suite_bit_utils)
     MU_RUN_TEST(test_bit_set);
     MU_RUN_TEST(test_bit_clr);
     MU_RUN_TEST(test_bit_chg);
+
     MU_RUN_TEST(test_fragment);
+
     MU_RUN_TEST(test_mask_below);
     MU_RUN_TEST(test_mask_below_inc);
     MU_RUN_TEST(test_mask_above);
     MU_RUN_TEST(test_mask_above_inc);
+
     MU_RUN_TEST(test_bin_parse);
     MU_RUN_TEST(test_bin_tostring);
 }
