6179da3033d3d28d87d042c768f9a8c24dfc8385
Merwan Achibet <achibet.merwan@gmail.com>
2017-05-18 12:31:33 +0200

    Fix MOVEM instruction

--- m68k/instructions_transfer.c
+++ m68k/instructions_transfer.c
@@ -154,7 +154,7 @@ int movem(Instruction* i)
                 uint32_t* reg = ea->type == AddressRegisterIndirectPreDec ? MOVEM_PREDEC_ORDER(m) : MOVEM_POSTINC_ORDER(m);
 
                 if (i->size == Word)
-                    reg = SIGN_EXTEND_W(*reg);
+                    *reg = SIGN_EXTEND_W(*reg);
 
                 m68k_write(i->context, i->size, offset, *reg);
             }
