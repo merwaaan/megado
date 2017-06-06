dd9e434c51ddf456c9e5186826db1932b24358d7
Merwan Achibet <achibet.merwan@gmail.com>
2017-06-06 10:49:44 +0200

    Show planes in a debug window

--- genesis/renderer.h
+++ genesis/renderer.h
@@ -3,6 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <stdbool.h>
 
+enum Planes;
+
 typedef struct Renderer
 {
     Genesis* genesis;
@@ -13,13 +15,18 @@ typedef struct Renderer
     bool show_vdp_palettes;
     bool show_vdp_patterns;
 
+    bool show_vdp_planes;
+    enum Planes selected_plane;
+    uint8_t* plane_buffer;
+
+    // Graphics resources for rendering the Genesis' output
     GLuint game_shader;
     GLuint game_texture;
     GLuint game_vertex_array_object;
-    //static int g_AttribLocationProjMtx2 = 0;
 
+    // Graphics resources for the user interface
     GLuint ui_shader;
-    GLuint ui_patterns_texture;
+    GLuint ui_patterns_texture, ui_planes_texture;
     GLuint ui_vertex_array_object, ui_vertex_buffer_object, ui_element_buffer_object;
     GLint ui_shader_texture_loc, ui_shader_projection_loc;
 
