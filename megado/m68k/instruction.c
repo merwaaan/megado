6cb842a6ffd01ee3abd18978698a9919c2fe19a0
Merwan Achibet <achibet.merwan@gmail.com>
2017-07-05 19:06:00 +0200

    Fix some invalid opcode patterns

--- megado/m68k/instruction.c
+++ megado/m68k/instruction.c
@@ -161,6 +161,12 @@ typedef struct Pattern
 #define MODES_ALL         (MODES_DATA | MODES_ADDR | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC | MODES_IMM)
 #define MODES_NONE        0
 
+// Note: some instructions have different legal addressing modes depending
+// on the size of the operation (eg. the address register is often not
+// allowed for byte-size operation). In this case, we HAVE to spell out
+// the legal modes for each size instead of using the S2/S3 tokens in the
+// opcode pattern.
+
 static Pattern instruction_patterns[] =
 {
    { "0000000000111100", &gen_ori_ccr },
@@ -175,17 +181,18 @@ static Pattern instruction_patterns[] =
    { "0000101001111100", &gen_eori_sr },
    { "00001010S2MMMXXX", &gen_eori, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "00001100S2MMMXXX", &gen_cmpi, MODES_ALL & ~(MODES_ADDR | MODES_IMM) },
-   { "0000100000MMMXXX", &gen_btst_imm, MODES_ALL & ~(MODES_ADDR | MODES_IMM) },
-   { "0000100000MMMXXX", &gen_bchg_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
-   { "0000100010MMMXXX", &gen_bclr_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
-   { "0000100011MMMXXX", &gen_bset_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
-   { "0000???100MMMXXX", &gen_btst, MODES_ALL & ~MODES_ADDR }, // SIZE
-   { "0000???101MMMXXX", &gen_bchg, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
-   { "0000???110MMMXXX", &gen_bclr, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
-   { "0000???111MMMXXX", &gen_bset, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // SIZE
+   { "0000100000MMMXXX", &gen_btst_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC },
+   { "0000100001MMMXXX", &gen_bchg_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
+   { "0000100010MMMXXX", &gen_bclr_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
+   { "0000100011MMMXXX", &gen_bset_imm, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
+   { "0000???100MMMXXX", &gen_btst, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC },
+   { "0000???101MMMXXX", &gen_bchg, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
+   { "0000???110MMMXXX", &gen_bclr, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
+   { "0000???111MMMXXX", &gen_bset, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "0000???1??001???", &gen_movep },
-   { "00S3???001MMMXXX", &gen_movea, MODES_ALL }, // SIZE
-   { "0001XXXMMMMMMXXX", &gen_move, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS, MODES_ALL & ~MODES_ADDR}, // Byte operation: no address register as source
+   { "0011???001MMMXXX", &gen_movea, MODES_ALL }, // Word (no byte-sized version)
+   { "0010???001MMMXXX", &gen_movea, MODES_ALL }, // Long
+   { "0001XXXMMMMMMXXX", &gen_move, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS, MODES_ALL & ~MODES_ADDR}, // Byte: no address register as source
    { "00S3XXXMMMMMMXXX", &gen_move, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS, MODES_ALL },
    { "0100000011MMMXXX", &gen_move_from_sr, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "0100010011MMMXXX", &gen_move_to_ccr,  MODES_ALL & ~MODES_ADDR },
@@ -198,10 +205,10 @@ static Pattern instruction_patterns[] =
    { "0100100000MMMXXX", &gen_nbcd, MODES_DATA | MODES_ADDR | MODES_ADDR_OFFSET | MODES_ABS },
    { "0100100001000???", &gen_swap },
    { "0100100001MMMXXX", &gen_pea, MODE_MASK(AddressRegisterIndirect) | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC },
-   { "0100101011111100", &gen_illegal }, // TODO
+   { "0100101011111100", &gen_illegal },
    { "0100101011MMMXXX", &gen_tas, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "01001010S2MMMXXX", &gen_tst, MODES_ALL & ~(MODES_ADDR | MODES_IMM) },
-   { "010011100100????", &gen_trap }, // TODO vec?
+   { "010011100100????", &gen_trap },
    { "0100111001010???", &gen_link },
    { "0100111001011???", &gen_unlk },
    { "010011100110????", &gen_move_usp },
@@ -217,22 +224,28 @@ static Pattern instruction_patterns[] =
    { "010011001?MMMXXX", &gen_movem, MODE_MASK(AddressRegisterIndirect) | MODE_MASK(AddressRegisterIndirectPostInc) | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC },
    { "0100???111MMMXXX", &gen_lea,  MODE_MASK(AddressRegisterIndirect) | MODES_ADDR_OFFSET | MODES_ABS | MODES_PC },
    { "0100???110MMMXXX", &gen_chk, MODES_ALL & ~MODES_ADDR },
-   { "0101???0S2MMMXXX", &gen_addq, MODES_ALL & ~(MODES_PC | MODES_IMM) }, // SIZE
-   { "0101???1S2MMMXXX", &gen_subq, MODES_ALL & ~(MODES_PC | MODES_IMM) }, // SIZE
+   { "0101???000MMMXXX", &gen_addq, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Byte: no address register
+   { "0101???001MMMXXX", &gen_addq, MODES_DATA | MODES_ADDR | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Word
+   { "0101???010MMMXXX", &gen_addq, MODES_DATA | MODES_ADDR | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Long
+   { "0101???100MMMXXX", &gen_subq, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Byte: no address register
+   { "0101???101MMMXXX", &gen_subq, MODES_DATA | MODES_ADDR | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Word
+   { "0101???110MMMXXX", &gen_subq, MODES_DATA | MODES_ADDR | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // Long
    { "0101????11MMMXXX", &gen_scc, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "0101????11001???", &gen_dbcc },
    { "01100000????????", &gen_bra },
    { "01100001????????", &gen_bsr },
-   { "0110????????????", &gen_bcc }, // clash? 
+   { "0110????????????", &gen_bcc }, 
    { "0111???0????????", &gen_moveq },
    { "1000???011MMMXXX", &gen_divu, MODES_ALL & ~MODES_ADDR },
    { "1000???111MMMXXX", &gen_divs, MODES_ALL & ~MODES_ADDR },
    { "1000???10000????", &gen_sbcd },
    { "1000???0S2MMMXXX", &gen_or, MODES_ALL & ~MODES_ADDR },
    { "1000???1S2MMMXXX", &gen_or, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
-   { "1001???0S2MMMXXX", &gen_sub, MODES_ALL }, // size subtlety
-   { "1001???1S2MMMXXX", &gen_sub, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // size subtlety, SUBA thing?
-   { "1001???1S200????", &gen_subx }, // clash?
+   { "1001???000MMMXXX", &gen_sub, MODES_ALL & ~MODES_ADDR }, // Byte: no address register
+   { "1001???001MMMXXX", &gen_sub, MODES_ALL }, // Word
+   { "1001???010MMMXXX", &gen_sub, MODES_ALL }, // Long
+   { "1001???1S2MMMXXX", &gen_sub, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },   
+   { "1001???1S200????", &gen_subx },
    { "1001????11MMMXXX", &gen_suba, MODES_ALL },
    { "1011???1S2MMMXXX", &gen_eor, MODES_DATA | MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
    { "1011???1S2001???", &gen_cmpm },
@@ -241,11 +254,13 @@ static Pattern instruction_patterns[] =
    { "1100???011MMMXXX", &gen_mulu, MODES_ALL & ~MODES_ADDR },
    { "1100???111MMMXXX", &gen_muls, MODES_ALL & ~MODES_ADDR },
    { "1100???10000????", &gen_abcd },
-   { "1100???1??00????", &gen_exg }, // M?
+   { "1100???1??00????", &gen_exg },
    { "1100???0S2MMMXXX", &gen_and, MODES_ALL & ~MODES_ADDR },
    { "1100???1S2MMMXXX", &gen_and, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
-   { "1101???0S2MMMXXX", &gen_add, MODES_ALL }, // SIZE
-   { "1101???1S2MMMXXX", &gen_add, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // ADDA?
+   { "1101???000MMMXXX", &gen_add, MODES_ALL & ~MODES_ADDR }, // Byte: no address register
+   { "1101???001MMMXXX", &gen_add, MODES_ALL }, // Word
+   { "1101???010MMMXXX", &gen_add, MODES_ALL }, // Long
+   { "1101???1S2MMMXXX", &gen_add, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS }, // TODO ADDA (w sign extension) for addr reg?
    { "1101???1S200????", &gen_addx },
    { "1101????11MMMXXX", &gen_adda, MODES_ALL },
    { "1110000?11MMMXXX", &gen_asd_mem, MODES_ADDR_IND | MODES_ADDR_OFFSET | MODES_ABS },
@@ -261,7 +276,7 @@ static Pattern instruction_patterns[] =
 static bool pattern_match(Pattern* pattern, uint16_t opcode)
 {
     assert(strlen(pattern->bits) == 16);
-    
+
     uint16_t ea_modes = pattern->legal_ea_modes;
     uint16_t opcode_fragment;
 
@@ -313,37 +328,37 @@ static bool pattern_match(Pattern* pattern, uint16_t opcode)
             return false;
 
             // MMMXXX/XXXMMM
-            case 'M':
-            case 'X':
+        case 'M':
+        case 'X':
 
-                opcode_fragment = FRAGMENT(opcode, 15 - cursor, 10 - cursor);
+            opcode_fragment = FRAGMENT(opcode, 15 - cursor, 10 - cursor);
 
-                // Swap the two halves if necessary
-                opcode_fragment = pattern->bits[cursor] == 'X' ?
-                    (opcode_fragment & 0b000111) << 3 | (opcode_fragment & 0b111000) >> 3 :
-                    opcode_fragment;
-               
-                if ((opcode_fragment & 0b111000) == 0b000000 && (ea_modes & MODE_MASK(DataRegister)) ||
-                    (opcode_fragment & 0b111000) == 0b001000 && (ea_modes & MODE_MASK(AddressRegister)) ||
-                    (opcode_fragment & 0b111000) == 0b010000 && (ea_modes & MODE_MASK(AddressRegisterIndirect)) ||
-                    (opcode_fragment & 0b111000) == 0b011000 && (ea_modes & MODE_MASK(AddressRegisterIndirectPostInc)) ||
-                    (opcode_fragment & 0b111000) == 0b100000 && (ea_modes & MODE_MASK(AddressRegisterIndirectPreDec)) ||
-                    (opcode_fragment & 0b111000) == 0b101000 && (ea_modes & MODE_MASK(AddressRegisterIndirectDisplacement)) ||
-                    (opcode_fragment & 0b111000) == 0b110000 && (ea_modes & MODE_MASK(AddressRegisterIndirectIndexed)) ||
-                    opcode_fragment == 0b111010 && (ea_modes & MODE_MASK(ProgramCounterDisplacement)) ||
-                    opcode_fragment == 0b111011 && (ea_modes & MODE_MASK(ProgramCounterIndexed)) ||
-                    opcode_fragment == 0b111000 && (ea_modes & MODE_MASK(AbsoluteShort)) ||
-                    opcode_fragment == 0b111001 && (ea_modes & MODE_MASK(AbsoluteLong)) ||
-                    opcode_fragment == 0b111100 && (ea_modes & MODE_MASK(Immediate)))
-                {
-                    cursor += 6;
-                    ea_modes = pattern->legal_ea_modes2;
-                    continue;
-                }
+            // Swap the two halves if necessary
+            opcode_fragment = pattern->bits[cursor] == 'X' ?
+                (opcode_fragment & 0b000111) << 3 | (opcode_fragment & 0b111000) >> 3 :
+                opcode_fragment;
 
-                // TODO check legal modes*/
+            if ((opcode_fragment & 0b111000) == 0b000000 && (ea_modes & MODE_MASK(DataRegister)) ||
+                (opcode_fragment & 0b111000) == 0b001000 && (ea_modes & MODE_MASK(AddressRegister)) ||
+                (opcode_fragment & 0b111000) == 0b010000 && (ea_modes & MODE_MASK(AddressRegisterIndirect)) ||
+                (opcode_fragment & 0b111000) == 0b011000 && (ea_modes & MODE_MASK(AddressRegisterIndirectPostInc)) ||
+                (opcode_fragment & 0b111000) == 0b100000 && (ea_modes & MODE_MASK(AddressRegisterIndirectPreDec)) ||
+                (opcode_fragment & 0b111000) == 0b101000 && (ea_modes & MODE_MASK(AddressRegisterIndirectDisplacement)) ||
+                (opcode_fragment & 0b111000) == 0b110000 && (ea_modes & MODE_MASK(AddressRegisterIndirectIndexed)) ||
+                opcode_fragment == 0b111010 && (ea_modes & MODE_MASK(ProgramCounterDisplacement)) ||
+                opcode_fragment == 0b111011 && (ea_modes & MODE_MASK(ProgramCounterIndexed)) ||
+                opcode_fragment == 0b111000 && (ea_modes & MODE_MASK(AbsoluteShort)) ||
+                opcode_fragment == 0b111001 && (ea_modes & MODE_MASK(AbsoluteLong)) ||
+                opcode_fragment == 0b111100 && (ea_modes & MODE_MASK(Immediate)))
+            {
+                cursor += 6;
+                ea_modes = pattern->legal_ea_modes2;
+                continue;
+            }
 
-                return false;
+            // TODO check legal modes*/
+
+            return false;
         }
 
         return false;
@@ -377,7 +392,7 @@ Instruction* instruction_make(char* name, InstructionFunc func)
     i->func = func;
     i->base_cycles = 0;
 
-    // Dynamically allocate memory for the instruction name
+    // Dynamically allocate memory for the instruction's name
     // and copy the name argument whatever its source in order
     // to handle string literals and dynamic strings similarly.
     i->name = calloc(20, sizeof(char));
@@ -399,6 +414,7 @@ void instruction_free(Instruction* instr)
 
 uint8_t instruction_execute(Instruction* instr, M68k* ctx)
 {
+    // TODO compare w inline noop
     // Pre-execution actions
     if (instr->src != NULL && instr->src->pre_func != NULL)
         instr->src->pre_func(instr->src, ctx);
