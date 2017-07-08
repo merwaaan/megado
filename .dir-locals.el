890682d27b72091b76a5e8a2c77f93353724bec1
fmdkdd <fmdkdd@gmail.com>
2017-07-08 23:52:43 +0200

    Add deps paths to flycheck-clang in .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil . ((c-basic-offset . 4)
-         )))
+((nil
+  (c-basic-offset . 4)
+  (flycheck-clang-include-path . ("../" "../deps/json-c/include/json-c" "../deps/glfw/include" "../glew/include" "../deps/cimgui"))
+  (flycheck-clang-language-standard . "c11")))
