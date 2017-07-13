3d3a3d92e0881ed3c7860a1fb287823fd2e63c97
fmdkdd <fmdkdd@gmail.com>
2017-07-13 13:11:46 +0200

    Update .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
-((nil
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
   (c-basic-offset . 4)
-  (flycheck-clang-include-path . ("../" "../deps/json-c/include/json-c" "../deps/glfw/include" "../glew/include" "../deps/cimgui"))
-  (flycheck-clang-language-standard . "c11")))
+  (flycheck-clang-include-path "../" "../deps/json-c/include/json-c" "../deps/glfw/include" "../deps/glew/include" "../deps/cimgui")
+  (flycheck-clang-language-standard . "c11")
+  (flycheck-check-syntax-automatically save)))
