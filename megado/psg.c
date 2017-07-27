7091af952817d9e8bc45a7cb6e125e53772b6828
fmdkdd <fmdkdd@0xc0de.fr>
2017-07-27 21:24:32 +0200

    psg: Writing to the registers

--- megado/psg.c
+++ megado/psg.c
@@ -3,6 +3,9 @@
 
 #include "psg.h"
 
+void psg_write_latch(PSG*, uint8_t);
+void psg_write_data(PSG*, uint8_t);
+
 PSG* psg_make(struct Genesis* g) {
     return calloc(1, sizeof(PSG));
 }
@@ -11,6 +14,66 @@ void psg_free(PSG* p) {
     free(p);
 }
 
+// Entry point for other chip to communicate with the PSG
 void psg_write(PSG* p, uint8_t value) {
-    printf("Unhandled PSG write: %x\n", value);
+    if (value & 0x80) {
+        p->latched_channel = value >> 5;
+        p->latched_register = value >> 4;
+        psg_write_latch(p, value);
+    } else {
+        psg_write_data(p, value);
+    }
+
+    for (int i=0; i < 3; ++i) {
+        printf("chan%d: %x (vol) %x (tone)\n", i, p->square[i].volume, p->square[i].tone);
+    }
+    printf("noise: %x (vol) %x (noise)\n", p->noise.volume, p->noise.noise);
+}
+
+// Write low nibble to the currently latched channel & register
+void psg_write_latch(PSG* p, uint8_t value) {
+    value = value & 0xf;
+    switch (p->latched_channel) {
+    case 0:
+    case 1:
+    case 2:
+        if (p->latched_register) {
+            p->square[p->latched_channel].volume = value;
+        } else {
+            p->square[p->latched_channel].tone = value;
+        }
+        break;
+
+    case 3:
+        if (p->latched_register) {
+            p->noise.volume = value;
+        } else {
+            p->noise.noise = value;
+        }
+        break;
+    }
+}
+
+// Write 6bit value to the currently latched channel & register
+void psg_write_data(PSG* p, uint8_t value) {
+    value = value & 0x3f;
+    switch (p->latched_channel) {
+    case 0:
+    case 1:
+    case 2:
+        if (p->latched_register) {
+            p->square[p->latched_channel].volume = value;
+        } else {
+            p->square[p->latched_channel].tone = value << 4;
+        }
+        break;
+
+    case 3:
+        if (p->latched_register) {
+            p->noise.volume = value;
+        } else {
+            p->noise.noise = value;
+        }
+        break;
+    }
 }
--- megado/psg.h
+++ megado/psg.h
@@ -2,10 +2,33 @@
 
 #include <stdint.h>
 
+// PSG: Programmable Sound Generator
+// see: http://www.smspower.org/Development/SN76489
+
 struct Genesis;
 
-typedef struct PSG {
+typedef struct SquareChannel {
+    uint8_t volume : 4;
+    uint16_t tone : 10;
+} SquareChannel;
 
+typedef struct NoiseChannel {
+    uint8_t volume : 4;
+    union {
+        uint8_t noise;
+        struct {
+            uint8_t mode : 1;
+            uint8_t shift_rate : 2;
+        };
+    };
+} NoiseChannel;
+
+typedef struct PSG {
+    SquareChannel square[3];
+    NoiseChannel noise;
+
+    uint8_t latched_channel : 2;
+    uint8_t latched_register : 1;
 } PSG;
 
 PSG* psg_make(struct Genesis*);
