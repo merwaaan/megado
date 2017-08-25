5348ed99ba21699a003acc49463ea7f1fe334129
fmdkdd <fmdkdd@gmail.com>
2017-08-25 14:20:22 +0200

    ym2612: Add registers to structs

--- megado/ym2612.h
+++ megado/ym2612.h
@@ -5,19 +5,58 @@
 
 // YM2612 - Main sound chip
 // http://md.squee.co/YM2612
+// http://www.smspower.org/maxim/Documents/YM2612
+
+typedef struct Operator {
+    uint8_t detune                       : 3;
+    uint8_t multiple                     : 4;
+    uint8_t total_level                  : 7;
+    uint8_t rate_scaling                 : 2;
+    uint8_t attack_rate                  : 5;
+    bool    amplitude_modulation_enabled : 1;
+    uint8_t first_decay_rate             : 5;
+    uint8_t second_decay_rate            : 5;
+    uint8_t secondary_amplitude          : 4;
+    uint8_t release_rate                 : 4;
+    // uint8_t ssg_eg                    : 4;  // proprietary register, skipping
+} Operator;
+
+typedef struct Frequency {
+    uint8_t  block     : 3;
+    uint16_t frequency : 11;
+} Frequency;
+
+typedef struct Channel {
+    Operator operators[4];
+
+    Frequency frequency;
+    uint8_t   feedback                         : 3;
+    uint8_t   algorithm                        : 3;
+    bool      left_output                      : 1;
+    bool      right_output                     : 1;
+    uint8_t   amplitude_modulation_sensitivity : 2;
+    uint8_t   frequency_modulation_sensitivity : 3;
+} Channel;
 
 typedef struct YM2612
 {
-    // TODO what does LFO mean?
-    bool lfo_enabled;
-    uint8_t lfo_frequency_index;
+    bool     lfo_enabled         : 1;
+    uint8_t  lfo_frequency_index : 3;
+    uint16_t timer_a             : 10;
+    uint8_t  timer_b             : 8;
+    uint8_t  channel3_mode       : 2;
+    uint8_t  dac_data            : 8;
+    bool     dac_enabled         : 1;
 
-    uint16_t timer_a;
-    uint16_t timer_b;
+    Channel channels[6];
 
-    uint8_t dac_value;
-    bool dac_enabled;
+    // These are only used by channels 3 & 6, but it's easier to keep them in
+    // the global struct:
+    Frequency channel3_additional_frequencies[3];
+    Frequency channel6_additional_frequencies[3];
+
 } YM2612;
+
 
 static float lfo_frequencies[] = {
     3.98,
