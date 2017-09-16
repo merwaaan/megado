441286ca96c37d8fff4377b2e1ca67fff5a05242
fmdkdd <fmdkdd@0xc0de.fr>
2017-09-16 10:21:27 +0200

    travis: Exit emulation after 5 minutes
    Since Travis kills job after 10min without output.

--- test/main.c
+++ test/main.c
@@ -1,20 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <megado/genesis.h>
 #include <megado/psg.h>
 #include <megado/settings.h>
 #include <megado/ym2612.h>
 #include <megado/m68k/instruction.h>
 #include <megado/m68k/m68k.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void run(Genesis* g, char* path)
 {
     genesis_load_rom_file(g, path);
 
 // Enable debug windows on CI to catch more bugs
-#ifdef ALL_WINDOWS
-    g->settings->vsync = false;
+#ifdef TRAVIS
+    g->settings->vsync = true;
+    g->settings->video_scale = 1f;
+    g->settings->rewinding_enabled = true;
+    g->settings->emulation_speed = 1f; // FIXME: we should as fast as we can on
+                                       // CI, as a fixed speed rate may already
+                                       // be too much for the VM.
+
     g->settings->show_m68k_registers = true;
     g->settings->show_m68k_disassembly = true;
     g->settings->show_m68k_log = true;
@@ -26,6 +34,8 @@ void run(Genesis* g, char* path)
     g->settings->show_vdp_patterns = true;
     g->settings->show_vdp_planes = true;
     g->settings->show_vdp_sprites = true;
+    g->settings->show_psg_registers = true;
+    g->settings->show_ym2612_registers = true;
     g->settings->show_rom = true;
     g->settings->show_ram = true;
     g->settings->show_vram = true;
@@ -34,12 +44,14 @@ void run(Genesis* g, char* path)
     g->settings->show_metrics = true;
 #endif
 
+    double start = glfwGetTime();
+
     while (g->status != Status_Quitting) {
         genesis_update(g);
 
-// Automatically exit after a few cycles; necessary for continuous integration
-#ifdef TIMEOUT
-        if (g->m68k->cycles > TIMEOUT) break;
+// Automatically exit after 5 minutes on continuous integration
+#ifdef TRAVIS
+        if (glfwGetTime() - start > 300) break;
 #endif
     }
 }
