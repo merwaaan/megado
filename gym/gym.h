b8f9cdfa592964586375ea2b7bc7e4514423ab6e
fmdkdd <fmdkdd@0xc0de.fr>
2018-06-07 12:55:51 +0200

    gym: Move GYM stuff to gym.h & gym.c

---
+++ gym/gym.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+
+struct GYM {
+  uint8_t *data;
+  uint64_t size;
+
+  struct GYMHeader {
+    char tag[4];
+    char song[32];
+    char game[32];
+    char copyright[32];
+    char emulator[32];
+    char dumper[32];
+    char comment[256];
+    uint32_t loop_start;
+    uint32_t packed_size;
+  } *header;
+};
+
+void gym_play(struct GYM gym);
+struct GYM read_gym(FILE *gym_file);
